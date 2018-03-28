c71374884ff454925d7a9fc37bd084ee25d3a36e
leelic07 <lichengchao1@sina.cn>
2018-03-28 18:07:01 +0800

    合并代码

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Layout from '@/views/common/Layout'
+import Layout from '@/views/common/layout'
 // import App from '@/app'
 import router from './router'
 import ElementUI from 'element-ui'
