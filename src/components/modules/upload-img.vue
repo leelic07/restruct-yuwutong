50d72ec9325dd848d6b09e56ee3731d82082893f
twmay006 <twmay006@163.com>
2018-05-25 14:04:34 +0800

    修改测试地址

--- src/components/modules/upload-img.vue
+++ src/components/modules/upload-img.vue
@@ -39,8 +39,8 @@ export default {
     action: {
       type: String,
       // default: 'http://39.108.185.51:1339/avatars'
-      default: `https://www.yuwugongkai.com/image-server/avatars`
-      // default: `http://123.57.7.159:1339/image-server/avatars` // 测试和演示
+      // default: `https://www.yuwugongkai.com/image-server/avatars`
+      default: `http://123.57.7.159:1339/image-server/avatars` // 测试和演示
     },
     headers: {
       type: Object,
