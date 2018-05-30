5a59c39bc72c419bfb57fd72f76cc1e190649211
leelic07 <330314702@qq.com>
2018-05-30 09:50:09 +0800

    家属会见申请记录

--- src/views/common/menu.js
+++ src/views/common/menu.js
@@ -58,11 +58,12 @@ let superAdminMenu = [{
         path: '/meeting/adjust',
         title: '会见申请调整',
         icon: 'iconfont icon-circle-o'
-      }, {
-        path: '/meeting/records',
-        title: '会见申请记录',
-        icon: 'iconfont icon-circle-o'
       }
+      // {
+      //   path: '/meeting/records',
+      //   title: '会见申请记录',
+      //   icon: 'iconfont icon-circle-o'
+      // }
     ]
   }, {
     path: '/visit/list',
